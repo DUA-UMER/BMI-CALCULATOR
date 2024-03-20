ae319f9522b3ccf64222dccdd0843bfc93e743f7
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-03-20 23:12:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// BMI CALCULATOR USING INQUIRER PROMPT
+import inquirer from "inquirer";
+const question = [
+    { type: 'number',
+        name: 'weight',
+        message: 'weight(kg)' },
+    { type: 'number',
+        name: 'height',
+        message: 'height(m)' },
+];
+const calculateBMI = (weight, height) => weight / (height * height);
+const main = async () => {
+    const { weight, height } = await inquirer.prompt(question);
+    console.log(`BMI: ${calculateBMI(weight, height).toFixed(4)}`);
+};
+main();
